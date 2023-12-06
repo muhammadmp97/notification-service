1818fdc592cf4de1a41494f0bf13a6019cb3b479
Muhammad M <muhammadmp1997@gmail.com>
2023-12-06 19:27:40 +0330

    templates support foreach loops

--- src/template-renderer.js
+++ src/template-renderer.js
@@ -3,9 +3,23 @@ import { promises as fs } from 'fs'
 const render = async (channel, template, data) => {
   let text = await fs.readFile(`./src/templates/${channel}/${template}.template`, 'utf8')
 
-  return text.replace(/{{ ([A-z0-9_-]+) }}/g, function (str, match) {
-    return data[match]
+  // Supporting loops
+  text = text.replace(/@foreach\((\S+) as (\S+)\)([\n]*)(.*)([\n]*)@endforeach\(\)/g, function (str, itterable, element, beforeContent, content, afterContent) {
+    let output = ''
+    let index = 0
+    for (let el of data[itterable]) {
+      output += content.replaceAll(element, `${itterable}[${index}]`) + afterContent
+      index++
+    }
+
+    return output
   })
+
+  text = text.replace(/{{ ([A-z0-9._-]+) }}/g, function (str, match) {
+    return eval(`data.${match}`)
+  })
+
+  return text
 }
 
 export default render
